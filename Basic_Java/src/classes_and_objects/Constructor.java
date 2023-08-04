8f95f0cd43520274f984c15da1f15842d3b65b46
VAMSHIDHAR DAWOOR <vamshidhar.20bce7285@vitap.ac.in>
2023-08-04 10:05:39 +0530

    BASIC JAVA

---
+++ Basic_Java/src/classes_and_objects/Constructor.java
@@ -0,0 +1,29 @@
+package classes_and_objects;
+
+class Calcc{
+	int __num1;
+	int __num2;
+	int __result;
+	
+	public Calcc() { // without parameter
+		__num1 = 5;
+		__num2 = 5;
+		System.out.println("In Constructor");
+	}
+	
+	public Calcc(int n) { // with parameter
+		System.out.println(n);
+	}
+	
+	public Calcc(double n) {
+		System.out.println((int)n);
+	}
+}
+
+public class Constructor {
+	public static void main(String[] args) {
+		Calcc obj = new Calcc(7.4); // constructor is also called
+		
+//		System.out.println(obj.__num1);
+	}
+}
---
+++ Basic_Java/src/method_overloading/MethodOverloading.java
@@ -0,0 +1,5 @@
+package method_overloading;
+
+public class MethodOverloading {
+
+}
