425a72522b8324fb855054500e8a646de12f5482
VAMSHIDHAR DAWOOR <vamshidhar.20bce7285@vitap.ac.in>
2023-08-20 21:48:06 +0530

    DSA

--- DSA/linkedlist/CircularLinkedList.java
+++ DSA/linkedlist/CircularLinkedList.java
@@ -14,6 +14,19 @@ public class CircularLinkedList {
     Node head = null;
     Node tail = null;
 
+    public void traverse() {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        do {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        } while (temp != head);
+        System.out.println();
+    }
+
     // add - first
     public void addFirst(String data) {
         Node newNode = new Node(data);
@@ -30,17 +43,77 @@ public class CircularLinkedList {
         tail.next = head;
     }
 
-    public void traverse() {
-        Node temp = head;
+    // add - end
+    public void addLast(String data) {
+        Node newNode = new Node(data);
         if (head == null) {
-            System.out.println("List is empty");
+            head = newNode;
+            tail = newNode;
+            newNode.next = head;
             return;
         }
-        do{
-            System.out.print(temp.data + " ");
+
+        tail.next = newNode;
+        tail = newNode;
+        newNode.next = head;
+    }
+
+    // add at location
+    public void addLoc(String data, int pos) {
+        Node newNode = new Node(data);
+        Node temp = head;
+        for (int i = 0; i < pos - 1; i++) {
             temp = temp.next;
         }
-        while (temp != head) ;
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    // delete - first
+    public void deleteFirst() {
+        if (head == null) {
+            System.out.println("List is Empty");
+            return;
+        }
+
+        Node temp = head;
+
+        temp = temp.next;
+        head = temp;
+        tail.next = head;
+    }
+
+    // delete - last
+    public void deleteLast() {
+        if (tail == null || head == null) {
+            System.out.println("List is Empty");
+            return;
+        }
+
+        Node temp = head;
+        Node ptr = temp.next;
+
+        while (ptr.next != head) {
+            temp = ptr;
+            ptr = ptr.next;
+        }
+
+        temp.next = head;
+        tail = temp;
+    }
+
+    // delete - location
+    public void deleteLoc(int pos) {
+        Node temp = head;
+        Node ptr = temp.next;
+
+        for (int i = 1; i < pos - 1; i++) {
+            temp = ptr;
+            ptr = ptr.next;
+        }
+
+        temp.next = ptr.next;
     }
 
     public static void main(String[] args) {
@@ -51,6 +124,21 @@ public class CircularLinkedList {
         cll.addFirst("D");
         cll.addFirst("E");
 
+        cll.traverse();
+
+        cll.addLast("F");
+        cll.traverse();
+
+        cll.addLoc("G", 3);
+        cll.traverse();
+
+        cll.deleteFirst();
+        cll.traverse();
+
+        cll.deleteLast();
+        cll.traverse();
+
+        cll.deleteLoc(2);
         cll.traverse();
     }
 }
