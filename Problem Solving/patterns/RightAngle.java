e552af0f7c36b94d6206890a96247b07d2580119
VAMSHIDHAR DAWOOR <vamshidhar.20bce7285@vitap.ac.in>
2023-08-20 21:48:29 +0530

    Patterns

--- Problem Solving/patterns/RightAngle.java
+++ Problem Solving/patterns/RightAngle.java
@@ -3,11 +3,21 @@ package patterns;
 public class RightAngle {
     public static void main(String[] args) {
         System.out.println("Right Angle Triangle");
-        
+
         int n = 6;
         for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= i+1; j++) {
+            for (int j = 1; j <= i + 1; j++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i + 1; k++) {
+                System.out.print("*");
             }
             System.out.println();
         }
