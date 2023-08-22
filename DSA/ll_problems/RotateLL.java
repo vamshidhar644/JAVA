fbfe38736e6543b7393016c26d4362bdde50fd4d
VAMSHIDHAR DAWOOR <vamshidhar.20bce7285@vitap.ac.in>
2023-08-22 17:12:47 +0530

    DSA

---
+++ DSA/ll_problems/RotateLL.java
@@ -0,0 +1,19 @@
+package ll_problems;
+
+class Node {
+    int data;
+    Node next;
+}
+
+class Linked{
+
+}
+
+public class RotateLL {
+    public static void main(String[] args) {
+        Linked obj = new Linked();
+        
+        Node root = null;
+        
+    }
+}
