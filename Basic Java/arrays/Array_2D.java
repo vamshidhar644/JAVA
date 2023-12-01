8f98c585903815a778a9f6f1baa6607040c3903f
VAMSHIDHAR DAWOOR <vamshidhar.20bce7285@vitap.ac.in>
2023-12-01 22:01:42 +0530

    Basic Java

--- Basic Java/arrays/Array_2D.java
+++ Basic Java/arrays/Array_2D.java
@@ -4,13 +4,13 @@ public class Array_2D {
 
 	public static void main(String[] args) {
 		int arr[][] = {
-						{1,2,3,4}, 
-						{2,4,4}, 
-						{3,6,5,7}
-					};
-		
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<arr[i].length;j++) {
+				{ 1, 2, 3, 4 },
+				{ 2, 4, 4 },
+				{ 3, 6, 5, 7 }
+		};
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
 				System.out.print(" " + arr[i][j]);
 			}
 			System.out.println();
